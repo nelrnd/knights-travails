1533b031acb56256efda7cc00e453ccb604b15c6
Nel <nel.renaudin@gmail.com>
2022-11-16 08:02:44 +0100

    Add function that gets every squares a knight can move to from any square in the chessboard

--- knights-travails.js
+++ knights-travails.js
@@ -21,3 +21,43 @@ const createChessboard = () => {
 const findNode = (chessboard, x, y) => {
   return chessboard.find((node) => node.x === x && node.y === y);
 };
+
+// get every squares a knight can move
+// from every square in the chessboard
+const getAllKnightMoves = () => {
+  // all the moves a knight can take
+  const moves = [
+    { x: 1, y: 2 },
+    { x: 2, y: 1 },
+    { x: 2, y: -1 },
+    { x: 1, y: -2 },
+    { x: -1, y: -2 },
+    { x: -2, y: -1 },
+    { x: -2, y: 1 },
+    { x: -1, y: 2 },
+  ];
+
+  // initialize a chessboard
+  const chessboard = createChessboard();
+
+  // for every square node in the chessboard
+  for (const node of chessboard) {
+    // get all its moves
+    for (const move of moves) {
+      // get new square from current square
+      const newX = node.x + move.x;
+      const newY = node.y + move.y;
+
+      // find corresponding node in chessboard
+      const newNode = findNode(chessboard, newX, newY);
+
+      // if move isn't possible, skip to next move
+      if (!newNode) break;
+
+      // else, link new node to current node
+      node.adjacents.push(newNode);
+    }
+  }
+
+  return chessboard;
+};
