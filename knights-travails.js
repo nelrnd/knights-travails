ea314d9621b9f6c8d3c1d8af90d76b20e2eb527b
Nel <nel.renaudin@gmail.com>
2022-11-16 07:54:13 +0100

    Add function that creates a chessboard

--- knights-travails.js
+++ knights-travails.js
@@ -1,3 +1,18 @@
 const SquareNode = (x, y) => {
   return { x, y, adjacents: [] };
 };
+
+// initialize chessboard with its square nodes
+const createChessboard = () => {
+  const chessboard = [];
+
+  // push 64 square nodes to chessboard
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      const node = SquareNode(x, y);
+      chessboard.push(node);
+    }
+  }
+
+  return chessboard;
+};
